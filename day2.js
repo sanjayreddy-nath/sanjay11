cc07dc007e0b1925320a4147b49aab7bc01b68c1
sanjayreddy-nath <sanjayreddy0305@gmail.com>
2024-12-10 21:09:22 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,38 @@
+// termenary operater
+// true/false?
+// Ture statement : false statement;
+
+var num1=2
+var num2=4
+num1<=num2 ? 
+console.log(num1,"is less than or equal to",num2): console.log(num1,"is greater than or equal to",num2)
+// OUTPUT: 2 is less than or equal to 4
+
+//assignment operater
+
+var a=2
+console.log(a+=4);  //6
+console.log(a*=5);  //30
+console.log(a/=5);  //6
+console.log(a-=2);  //4
+
+// logic operater
+
+if (15 && 15){
+    console.log("logical && satisfied");
+}
+else
+{
+    console.log("logical && is not satisfied");
+}
+// OUTPUT: logical && satisfied
+
+if (null || undefined){
+    console.log("logical || satisfied");
+}
+else
+{
+    console.log("logical || is not satisfied");
+}
+// OUTPUT: logical || is not satisfied
+
